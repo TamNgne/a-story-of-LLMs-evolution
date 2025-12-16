d900b1c2421148d094ac29958315f145fa40bc25
cryborg1211 <caokhuong12311@gmail.com>
2025-12-16 23:05:41 +0700

    fix Comparision chart and add to playground.jsx

--- client/src/pages/PlaygroundPage/PlaygroundPage.jsx
+++ client/src/pages/PlaygroundPage/PlaygroundPage.jsx
@@ -2,11 +2,13 @@
 import React from 'react';
 import TaskSpecializationChart from '../../components/TaskSpecializationChart/TaskSpecializationChart';
 import BenchmarkDashboard from '../../components/BenchmarkDashboard/BenchmarkDashboard';
+import ComparisonChart from '../../components/ComparisonChart/ComparisonChart';
 import styles from './PlaygroundPage.module.css';
 
 const PlaygroundPage = () => {
   return (
     <div className={styles.container}>
+      <ComparisonChart />
       <TaskSpecializationChart />
       <BenchmarkDashboard />
     </div>
